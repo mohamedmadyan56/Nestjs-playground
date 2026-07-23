4130a1064bf5f9ce140e29730019d0f27a501563
mohamed madyan <mohamedmadyan31@gmail.com>
2026-07-24 00:27:31 +0300

    first Controller and first Route In Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	controllers: [UserController],
 })
 export class AppModule {}
