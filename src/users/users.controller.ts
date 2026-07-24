9d115efd809b453012b0e51605ff3d5e342f9faa
mohamed madyan <mohamedmadyan31@gmail.com>
2026-07-24 03:57:43 +0300

    find and findONe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,25 +8,43 @@ import {
   Body,
   HttpCode,
   HttpStatus,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-User.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserEntity } from 'src/user.entity';
+import { v4 as uuid } from 'uuid';
 
 @Controller('users')
 export class UserController {
-  @Get()
-  find(): string[] {
-    return ['ahmed', 'mohamed', 'madyan'];
-  }
 
-  @Get(':username')
-  findOne(@Param('username') username: string): string {
-    return username;
+  private  readonly users:UserEntity[] = [];
+
+  @Get()
+  find(): UserEntity[] {
+    return this.users;
+  } 
+
+  @Get(':id')
+  findOne(@Param('id') id: string): UserEntity {
+    const user = this.users.find((user) => user.id === id);
+
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+
+    return user;
   }
 
   @Post()
   create(@Body() CreateUserDto: CreateUserDto) {
-    return CreateUserDto;
+    const newUser:UserEntity={
+      ...CreateUserDto,
+      id:uuid(),
+
+    }
+    this.users.push(newUser);
+    return newUser;
   }
 
   @Patch(':username')
