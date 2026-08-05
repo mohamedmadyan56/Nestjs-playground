673a8a41e234abfa688157a61937546f3b1b43e0
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-06 02:52:42 +0300

    User

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,21 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './users.controller';
-import { UserService } from './users.service';
+import { Controller, Get, Query, Param } from "@nestjs/common"
+@Controller('users')
+export class UserController {
+    @Get(':id')
+    getUsers(
+        @Param('id') id: any,
+        @Query('limit') limit: any,   // query param
+        @Query('page') page: any,
 
-class MockUserService {
-    findUser() {
-        return ['user1', 'user2'];
+    ) {
+        return 'User Founded';
     }
 }
-@Module({
-    controllers: [UserController],
-    providers: [
-        {
-            provide: UserService,
-            useValue: new MockUserService(),
-
-        }
-    ]
-})
-
-export class UserModule { }
