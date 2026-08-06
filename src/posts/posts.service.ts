4001f95a4aa55c5553b9228f0ebd1c568bbe16fd
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-06 19:46:30 +0300

    Play 41. Return Posts From Posts Service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,5 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PostsService {
-    public findAll(userId: string) { }
+    public findAll(userId: string) {
+        return [{
+            "title": "Test title 1",
+            "content": "Test content 1"
+        },
+        {
+            "title": "Test title 2",
+            "content": "Test content 2"
+        }]
+    }
 }
