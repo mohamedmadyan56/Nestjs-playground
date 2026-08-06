e4c1f082a6550da72f6570f489f5887c59d7bd63
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-06 04:40:55 +0300

    Validation ParseIntPipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,10 +4,11 @@ export class UserController {
   @Get(':id')
   getUsers(
     @Param('id', ParseIntPipe) id: number,
-    @Query('limit') limit: any,
-    @Query('page') page: any,
+    @Query('limit', ParseIntPipe) limit: number,
+    @Query('page', ParseIntPipe) page: number,
   ) {
-    console.log({ limit, page })
+    console.log({ limit, typeofLimit: typeof limit, page, typeofPage: typeof page });
+
     return 'user found'
   }
 }
