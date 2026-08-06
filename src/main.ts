f90b8e3cb73000c9abcaed354b15baff19071c40
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-07 02:42:56 +0300

    setDescription

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
 
-  const config = new DocumentBuilder().setTitle('NestJS Master Class Blog App API').setVersion('1.0').build();
+  const config = new DocumentBuilder().setTitle('NestJS Master Class Blog App API').setDescription('Use API URL as http://localhost:3000').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
