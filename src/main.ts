0996e16440069dfcb625be77bc903438e0702782
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-07 02:46:39 +0300

    setLiecenese

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
 
-  const config = new DocumentBuilder().setTitle('NestJS Master Class Blog App API').setDescription('Use API URL as http://localhost:3000').setTermsOfService('https://example.com/terms').setVersion('1.0').build();
+  const config = new DocumentBuilder().setTitle('NestJS Master Class Blog App API').setLicense('MIT', 'https://github.com/mit-license').setDescription('Use API URL as http://localhost:3000').setTermsOfService('https://example.com/terms').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
