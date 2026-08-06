bd2fd1d42460111cc71bb4dae28fe8a2c6b34f4c
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-06 04:46:19 +0300

    Validation ParseIntPipe and DefaultValudePipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Query, Param, ParseIntPipe } from "@nestjs/common"
+import { Controller, Get, Query, Param, ParseIntPipe, DefaultValuePipe } from "@nestjs/common"
 @Controller('users')
 export class UserController {
   @Get(':id')
   getUsers(
     @Param('id', ParseIntPipe) id: number,
-    @Query('limit', ParseIntPipe) limit: number,
-    @Query('page', ParseIntPipe) page: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
     console.log({ limit, typeofLimit: typeof limit, page, typeofPage: typeof page });
 
