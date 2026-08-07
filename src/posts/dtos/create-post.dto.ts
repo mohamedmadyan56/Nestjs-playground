c28889ce57440e34bcef881b1c31443fb238db22
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-07 05:35:16 +0300

    Play 53. Adding Validations To CreatePostDto

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,3 +1,4 @@
+import { IsEnum, IsISO8601, IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength } from "class-validator";
 import { postStatus } from "../enums/postStatus.enum";
 import { postType } from "../enums/postyType.enum";
 
@@ -5,14 +6,41 @@ import { postType } from "../enums/postyType.enum";
 
 
 export class CreatePostDto {
+    @IsString()
+    @MinLength(4)
+    @IsNotEmpty()
     title: string;
+    @IsEnum(postType)
+    @IsNotEmpty()
     postType: postType;
+    @IsString()
+    @IsNotEmpty()
+    @Matches(/^[a-z0-9-]+$/, {
+        message:
+            'A slug should be all small letters and uses only hyphen and without spaces.',
+    })
     slug: string;
+    @IsEnum(postStatus)
+    @IsNotEmpty()
+
     status: postStatus;
+    @IsString()
+    @IsOptional()
     content?: string;
+    @IsString()
+    @IsOptional()
+    @IsJSON()
     schema?: string;
+    @IsOptional()
+    @IsUrl()
     featuredImageUrl?: string;
+    @IsOptional()
+    @IsISO8601()
     publishedOn: Date;
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true })
+    @MinLength(3, { each: true })
     tags?: string[];
     metaOptions: [{ key: 'sidebarEnabled'; value: true }]
 
