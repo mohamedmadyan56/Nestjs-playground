34fbe6abc750565442bc0f57699ace94a2f5881c
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-09 01:38:15 +0300

    Connection Nest To Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,17 @@ import { Module } from "@nestjs/common";
 import { UserModule } from "./users/users.module";
 import { PostsModule } from './posts/providers/posts.module';
 import { AuthModule } from './auth/auth.module';
-
+import { TypeOrmModule } from "@nestjs/typeorm";
 @Module({
-    imports: [UserModule, PostsModule, AuthModule],
+    imports: [UserModule, PostsModule, AuthModule, TypeOrmModule.forRoot({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'password',
+        database: 'nestjs-blog'
+    })],
 })
 export class AppModule { }
