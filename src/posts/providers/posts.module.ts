7fa04ae37de0d6347070e6cee45c9da3380d87d1
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-10 13:29:26 +0300

    Refactroing 2

--- src/posts/providers/posts.module.ts
+++ src/posts/providers/posts.module.ts
@@ -1,8 +1,9 @@
-import { Module, Post } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from '../posts.service';
 import { UserModule } from '../../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from '../post.entity';
 
 @Module({
   controllers: [PostsController],
